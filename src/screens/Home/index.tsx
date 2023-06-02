ee4d9304ce73e28f54fa34d3389294cf40fd6bc9
Luis Oliveira <luis.oliveira@e14.io>
2023-06-01 23:36:59 -0300

    chore: header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import { Image, View, Text, ImageSourcePropType } from "react-native";
 import LogoImage from "../../images/logo.png";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import Avatar from "../../components/Avatar";
+import { Header } from "./styles";
 
 const Home: React.FC = () => {
   const imageSource: ImageSourcePropType = LogoImage;
 
   return (
     <SafeAreaView>
-      <Image
-        source={imageSource}
-        style={{ width: "40%" }}
-        resizeMode="contain"
-      />
-      <Text>Home page</Text>
+      <StatusBar style="dark" />
+      <Header>
+        <Avatar />
+        <Image
+          source={imageSource}
+          style={{ width: "40%" }}
+          resizeMode="contain"
+        />
+        <View style={{ width: 45 }}></View>
+      </Header>
     </SafeAreaView>
   );
 };
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 25px;
+`;
